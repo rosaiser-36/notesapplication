09d31ff1ff837330e60400ecd0f311ad9435ffcb
Jenkins <roshancpoojari989@gmail.com>
2024-07-14 11:46:57 +0000

    updated script post final

--- automate.sh
+++ automate.sh
@@ -1,3 +1,12 @@
+#########
+# date 14 -07 -2024
+# author Roshn
+# to run the python application using docker
+##############
+
+
 docker build -t $1 .
 
 docker run -d -p $2:3000 $1
+
+echo ' Your docker is running at localhost:$2 and image name is $1'
