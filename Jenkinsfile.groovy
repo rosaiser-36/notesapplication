26bd8ce05c546cb69f8e923623502b0f0b6acb69
Jenkins <roshancpoojari989@gmail.com>
2024-07-20 07:49:32 +0000

    clone and build are added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,11 +2,14 @@ pipeline {
     agent any
 
     stages {
-        stage('Build') {
+
+        stage('clone and build'){
             steps {
-                echo 'Building..'
+                echo 'cloning'
+                git url: 'https://github.com/rosaiser-36/notesapplication.git' , branch: 'main'
+                sh 'docker build -t jenkinsappimage .'             
             }
-        }
+     
         stage('Test') {
             steps {
                 echo 'Testing..'
